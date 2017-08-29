246204f39f1cdf74c9546654014f6d9ac5d78286
TropicalCat <xuxu.beibei@hotmail.com>
2017-08-29 08:07:30 +0800

    框架完成

--- Assets/Snaker/AppMain.cs
+++ Assets/Snaker/AppMain.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SGF;
+using Snaker.Service.Core.Example;
 
 public class AppMain : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		Example exp = new Example ();
+		exp.Start ();
 		
 	}
 	
--- Assets/Snaker/Service/Core/GlobalEvent.cs
+++ Assets/Snaker/Service/Core/GlobalEvent.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Snaker.Service.Core
 {
-	public class GlobaleEvent 
+	public class GlobalEvent 
 	{
 		public static ModuleEvent<bool> onLogin = new ModuleEvent<bool>();
 		public static ModuleEvent<bool> onPay = new ModuleEvent<bool>();
