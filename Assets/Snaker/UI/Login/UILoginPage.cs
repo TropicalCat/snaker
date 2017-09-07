7c233dca7682c60036e6ae580409c0022d7448a1
徐旭 <xubeibei1@xiaomi.com>
2017-09-07 15:02:21 +0800

    整理代码

---
+++ Assets/Snaker/UI/Login/UILoginPage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Snaker.Module;
+using Snaker.Service.UserManager.Data;
+using SGF.UI.Framework;
+using Snaker.Module.Framework;
+
+
+
+namespace Snaker.UI.Login
+{
+	public class UILoginPage : UIPage
+	{
+		public InputField inputId;
+		public InputField inputName;
+
+		protected override void OnOpen(object arg)
+		{
+			base.OnOpen (arg);
+			//UserData ud = AppConfig.Value.mainUserData;
+			//inputName.text = ud.name;
+			//inputId.text = ud.id.ToString ();
+		}
+
+		public void OnBtnLogin()
+		{
+			uint userId = 0;
+			uint.TryParse (inputId.text, out userId);
+			string userName = inputName.text.Trim ();
+			if (userId == 0) 
+			{
+				userId = (uint)Random.Range (100000, 999999);
+			}
+
+			//var module = ModuleManager.Instance.GetModule (ModuleDef.LoginModule) as LoginModule;
+			//if (module != null)
+			//{
+			//	module.Login(userId, userName, "");
+			//}
+		}
+	
+
+	}
+}
