398c9a92fc87c644ea30b6d623c8571bcae4e6c7
徐旭 <xubeibei1@xiaomi.com>
2017-09-07 15:01:27 +0800

    整理代码

---
+++ Assets/SGF/UI/Component/UIComponent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SGF.UI.Component
+{
+	public class UIComponent : MonoBehaviour 
+	{
+		protected virtual void Awake()
+		{
+		}
+
+		public virtual void SetData(object data)
+		{
+		}
+
+		public void SetVisible(bool value)
+		{
+			this.gameObject.SetActive (value);
+		}
+
+		public static void SetAVisible(UIBehaviour ui, bool value)
+		{
+			if(ui != null)
+			{
+				ui.gameObject.SetActive (value);
+			}
+		}
+
+		public bool IsVisible{ get { return this.gameObject.activeSelf;  } }
+
+		public static bool GetVisible(UIBehaviour ui)
+		{
+			return ui.gameObject.activeSelf;
+		}
+
+
+	}
+}
---
+++ Assets/SGF/UI/Component/UIListItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SGF.UI.Component
+{
+	public class UIListItem : UIComponent 
+	{
+		public UIListItem()
+		{
+		}
+
+		public virtual void UpdateItem(int index, object data)
+		{
+		}
+
+	}
+}
